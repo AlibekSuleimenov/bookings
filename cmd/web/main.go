2e9016e5a30a65474e70ef930c3992b716e6760b
Alibek Suleimenov <alibek443@gmail.com>
2023-12-25 15:37:46 +0600

    added tests for main middleware and routes

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -22,6 +22,25 @@ var session *scs.SessionManager
 
 // main is the main application function
 func main() {
+	err := run()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Starting an app on port", portNumber)
+
+	srv := &http.Server{
+		Addr:    portNumber,
+		Handler: routes(&app),
+	}
+
+	err = srv.ListenAndServe()
+
+	log.Fatal(err)
+}
+
+func run() error {
 	// what I am going to put in session
 	gob.Register(models.Reservation{})
 	// change this to true when in production
@@ -37,7 +56,7 @@ func main() {
 	tc, err := render.CreateTemplateCache()
 
 	if err != nil {
-		log.Fatal("cannot create template cache")
+		return err
 	}
 
 	app.TemplateCache = tc
@@ -47,15 +66,5 @@ func main() {
 
 	handlers.NewHandlers(repo)
 	render.NewTemplates(&app)
-
-	fmt.Println("Starting an app on port", portNumber)
-
-	srv := &http.Server{
-		Addr:    portNumber,
-		Handler: routes(&app),
-	}
-
-	err = srv.ListenAndServe()
-
-	log.Fatal(err)
+	return nil
 }
