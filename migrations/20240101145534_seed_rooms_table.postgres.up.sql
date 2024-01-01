cbcd161579c41c37d1bd2ab2f5ed8683e5339fab
Alibek Suleimenov <alibek443@gmail.com>
2024-01-01 21:07:57 +0600

    added seeders

---
+++ migrations/20240101145534_seed_rooms_table.postgres.up.sql
@@ -0,0 +1,2 @@
+INSERT INTO public.rooms (id, room_name, created_at, updated_at) VALUES (1, 'General''s Quarters', '2023-12-31 00:14:48.988693', '2023-12-31 00:14:48.988693');
+INSERT INTO public.rooms (id, room_name, created_at, updated_at) VALUES (2, 'Major''s Suite', '2023-12-31 00:14:48.988000', '2023-12-31 00:14:48.988000');
---
+++ migrations/20240101145953_seed_restrictions_table.postgres.up.sql
@@ -0,0 +1,2 @@
+INSERT INTO public.restrictions (id, restriction_name, created_at, updated_at) VALUES (1, 'Reservation', '2023-12-31 01:15:10.859997', '2023-12-31 01:15:10.859997');
+INSERT INTO public.restrictions (id, restriction_name, created_at, updated_at) VALUES (2, 'Owner Block', '2023-12-31 21:13:18.232774', '2023-12-31 21:13:18.232774');
